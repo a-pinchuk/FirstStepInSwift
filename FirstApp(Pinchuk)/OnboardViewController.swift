1acd949d13f46ae931814b26b72ca7900ae13878
Andrew Pinchuk <apinchuk@192.168.1.105.local.ipnet.ua>
2017-02-27 01:19:03 +0200

    Fixed troubles with view

--- FirstApp(Pinchuk)/OnboardViewController.swift
+++ FirstApp(Pinchuk)/OnboardViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 import PaperOnboarding
 
+import Foundation
+import UIKit
+
+extension UIImage {
+    enum Asset: String {
+        case Banks = "Banks"
+        case Hotels = "Hotels"
+        case Key = "Key"
+        case Shopping_Cart = "Shopping-cart"
+        case Stores = "Stores"
+        case Wallet = "Wallet"
+        
+        var image: UIImage {
+            return UIImage(asset: self)
+        }
+    }
+    
+    convenience init!(asset: Asset) {
+        self.init(named: asset.rawValue)
+    }
+}
+
+
 class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperOnboardingDelegate{
-
-@IBOutlet weak var onboardingView: OnboardingView!
-
+    
+    @IBOutlet weak var onboardingView: OnboardingView!
+    
+    
+    // Эта переменная будет изменяться при смене активного экрана, и так как индексация с 0, значит стандарнтое значение ее должно быть 0
+    var activeScreen = 0
     
     
     override func viewDidLoad() {
@@ -25,7 +51,8 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
         return 3
     }
     
-    func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
+    func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo
+    {
         let backgroundColorOne = UIColor(red: 217/255, green: 72/255, blue: 89/255, alpha: 1)
         let backgroundColorTwo = UIColor(red: 106/255, green: 166/255, blue: 211/255, alpha: 1)
         let backgroundColorThree = UIColor(red: 168/255, green: 200/255, blue: 78/255, alpha: 1)
@@ -33,10 +60,15 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
         let titleFont = UIFont(name: "AvenirNext-Bold", size: 24)!
         let descriptionFont = UIFont(name: "AvenirNext-Regular", size: 18)!
         
-
-        return [("rocket", "A Great Rocket Start", "Privet Privet", "", backgroundColorOne, UIColor.white, UIColor.white, titleFont, descriptionFont),
-                ("brush", "Design your experience", "Privet Privet", "", backgroundColorTwo, UIColor.white, UIColor.white, titleFont, descriptionFont),
-                ("cat", "Stay Up to Date", "Get norified of important updates", "", backgroundColorThree, UIColor.white, UIColor.white, titleFont, descriptionFont)][index]
+//        return [
+//            (UIImage.Asset.Hotels.rawValue, "rocket", "All hotels and hostels are sorted by hospitality rating", UIImage.Asset.Key.rawValue, UIColor(red:0.40, green:0.56, blue:0.71, alpha:1.00), UIColor.white, UIColor.white, titleFont,descriptionFont),
+//            (UIImage.Asset.Banks.rawValue, "brush", "We carefully verify all banks before add them into the app", UIImage.Asset.Wallet.rawValue, UIColor(red:0.40, green:0.69, blue:0.71, alpha:1.00), UIColor.white, UIColor.white, titleFont,descriptionFont),
+//            (UIImage.Asset.Stores.rawValue, "cat", "All local stores are categorized for your convenience", UIImage.Asset.Shopping_Cart.rawValue, UIColor(red:0.61, green:0.56, blue:0.74, alpha:1.00), UIColor.white, UIColor.white, titleFont,descriptionFont)
+//            ][index]
+        
+        return [("Limousine", "A Great Rocket Start", "Privet Privet", UIImage.Asset.Key.rawValue, backgroundColorOne, UIColor.white, UIColor.white, titleFont, descriptionFont),
+                ("brush", "Design your experience", "Privet Privet", UIImage.Asset.Wallet.rawValue, backgroundColorTwo, UIColor.white, UIColor.white, titleFont, descriptionFont),
+                ("cat", "Stay Up to Date", "Get norified of important updates", UIImage.Asset.Shopping_Cart.rawValue, backgroundColorThree, UIColor.white, UIColor.white, titleFont, descriptionFont)][index]
     }
     
     
@@ -46,44 +78,35 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
     
     @IBOutlet weak var getStartedButton: UIButton!
     
-//    @IBAction func goto(_ sender: Any ) {
-//        
-//    }
-    
-    func onboardingWillTransitonToIndex(_ index: Int) {
-//        if index == 1
-//        {
-//            self.getStartedButton.alpha = 1
-//
-//        }
-//        print(index)
-       
-    }
-    
-    func onboardingDidTransitonToIndex(_ index: Int) {
-        let activeScreen = index
-        
+    @IBAction func goto(_ sender: Any ) {
         switch activeScreen {
-        case 1:
+        case 0:
             self.performSegue(withIdentifier: "from1", sender: nil)
+            print("0_case")
+        case 1:
+            self.performSegue(withIdentifier: "from2", sender: nil)
             print("1_case")
         case 2:
-            self.performSegue(withIdentifier: "from2", sender: nil)
-             print("2_case")
-        case 3:
             self.performSegue(withIdentifier: "from3", sender: nil)
-             print("3_case")
+            print("2_case")
         default:
             print("Not the letter A")
         }
-
-        if index == 2
-        {
-            
-            UIView.animate(withDuration: 0.4, animations: {
-            self.getStartedButton.alpha = 1
-                            })
-        }
+    }
+    
+    func onboardingWillTransitonToIndex(_ index: Int) {
+        
+    }
+    
+    func onboardingDidTransitonToIndex(_ index: Int) {
+        activeScreen = index
+        //        if index == 2
+        //        {
+        //
+        //            UIView.animate(withDuration: 0.4, animations: {
+        //                self.getStartedButton.alpha = 1
+        //            })
+        //        }
     }
     
     
@@ -92,15 +115,15 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
